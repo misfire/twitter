b427fee75b251e7dedf5ca211ce81cf0b578fba2
Danny Chen <aznlucidx@gmail.com>
2013-06-04 23:59:33 -0700

    last 5 tweets :)

--- index.php
+++ index.php
@@ -1,37 +1,3 @@
-<!DOCTYPE html>
-<html>
-  <head>
-    <title>Bootstrap 101 Template</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <!-- Bootstrap -->
-    <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet" media="screen">
-  </head>
-  <body>
-    <!-- Part 1: Wrap all page content here -->
-    <div id="wrap">
-
-      <!-- Begin page content -->
-      <div class="container">
-        <div class="page-header">
-          <h1>Sticky footer</h1>
-        </div>
-        <p class="lead">Pin a fixed-height footer to the bottom of the viewport in desktop browsers with this custom HTML and CSS.</p>
-        <p>Use <a href="./sticky-footer-navbar.html">the sticky footer</a> with a fixed navbar if need be, too.</p>
-      </div>
-
-      <div id="push"></div>
-    </div>
-
-    <div id="footer">
-      <div class="container">
-        <p class="muted credit">Example courtesy <a href="http://martinbean.co.uk">Martin Bean</a> and <a href="http://ryanfait.com/sticky-footer/">Ryan Fait</a>.</p>
-      </div>
-    </div>
-    <script src="http://code.jquery.com/jquery.js"></script>
-    <script src="bootstrap/js/bootstrap.min.js"></script>
-  </body>
-</html>
-
 <?php
 
 require 'app_tokens.php';
@@ -44,8 +10,8 @@ $connection = new tmhOAuth(array('consumer_key'=> $consumer_key, 'consumer_secre
 
 //Get @justinbieber's account info
 
-$connection->request('GET',$connection->url('1.1/users/show'),
-	array('screen_name'=>'justinbieber'));
+$connection->request('GET',$connection->url('1.1/statuses/user_timeline'),
+  array('screen_name'=>'dannychenmusic'));
 
 //get HTTP response code for api request
 
@@ -57,15 +23,125 @@ $response_data = json_decode($connection->response['response'],true);
 
 // 200 success
 if($response_code<>200){
-	print "Error: $response_code\n";
+  print "Error: $response_code\n";
 
 }
 
 // Display the response array
 
-print_r($response_data);
-
-print($response_data['status']['text']);
+//print_r($response_data);
 
 
 ?>
+
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Bootstrap 101 Template</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Bootstrap -->
+    <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet" media="screen">
+ 
+
+  </head>
+  <body>
+    <!-- Part 1: Wrap all page content here -->
+    <div id="wrap">
+
+      <!-- Begin page content -->
+      <div class="container">
+        <div class="page-header">
+
+          <h1>Danny's Twitter Page</h1>
+             <ul class="nav nav-tabs">
+      <li class="active">
+        <a href="#">Twitter</a>
+        </li>
+        <li><a href="#">Google Maps</a></li>
+        <li><a href="#">Instagram</a></li>
+        </div>
+        <p class="lead">This shows the latest five tweets from my twitter.</p>
+         
+<div class="media">
+  <a class="pull-left" href="#">
+    <img class="media-object" data-src="holder.js/64x64" src="http://a0.twimg.com/profile_images/3304148517/ca5b7a3855c7293f78bd755373656146_normal.jpeg">
+  </a>
+  <div class="media-body">
+    <h4 class="media-heading"><?php echo "<a href=" . "http://www.twitter.com/" . $response_data[0]['user']['screen_name'] . ">" . $response_data[0]['user']['name'] . "</a>" ?></h4>
+    
+    <?php echo $response_data[0]['text'] ?>
+
+
+       <p> <?php echo date("l M j \- g:ia",strtotime($response_data['created_at'])); ?> </p>
+  </div>
+</div>
+
+<div class="media">
+  <a class="pull-left" href="#">
+    <img class="media-object" data-src="holder.js/64x64" src="http://a0.twimg.com/profile_images/3304148517/ca5b7a3855c7293f78bd755373656146_normal.jpeg">
+  </a>
+  <div class="media-body">
+    <h4 class="media-heading"><?php echo "<a href=" . "http://www.twitter.com/" . $response_data[1]['user']['screen_name'] . ">" . $response_data[1]['user']['name'] . "</a>" ?></h4>
+    
+    <?php echo $response_data[1]['text'] ?>
+
+
+       <p> <?php echo date("l M j \- g:ia",strtotime($response_data['created_at'])); ?> </p>
+  </div>
+</div>
+
+<div class="media">
+  <a class="pull-left" href="#">
+    <img class="media-object" data-src="holder.js/64x64" src="http://a0.twimg.com/profile_images/3304148517/ca5b7a3855c7293f78bd755373656146_normal.jpeg">
+  </a>
+  <div class="media-body">
+    <h4 class="media-heading"><?php echo "<a href=" . "http://www.twitter.com/" . $response_data[2]['user']['screen_name'] . ">" . $response_data[2]['user']['name'] . "</a>" ?></h4>
+    
+    <?php echo $response_data[2]['text'] ?>
+
+
+       <p> <?php echo date("l M j \- g:ia",strtotime($response_data['created_at'])); ?> </p>
+  </div>
+</div>
+
+<div class="media">
+  <a class="pull-left" href="#">
+    <img class="media-object" data-src="holder.js/64x64" src="http://a0.twimg.com/profile_images/3304148517/ca5b7a3855c7293f78bd755373656146_normal.jpeg">
+  </a>
+  <div class="media-body">
+    <h4 class="media-heading"><?php echo "<a href=" . "http://www.twitter.com/" . $response_data[3]['user']['screen_name'] . ">" . $response_data[3]['user']['name'] . "</a>" ?></h4>
+    
+    <?php echo $response_data[3]['text'] ?>
+
+
+       <p> <?php echo date("l M j \- g:ia",strtotime($response_data['created_at'])); ?> </p>
+  </div>
+</div>
+
+<div class="media">
+  <a class="pull-left" href="#">
+    <img class="media-object" data-src="holder.js/64x64" src="http://a0.twimg.com/profile_images/3304148517/ca5b7a3855c7293f78bd755373656146_normal.jpeg">
+  </a>
+  <div class="media-body">
+    <h4 class="media-heading"><?php echo "<a href=" . "http://www.twitter.com/" . $response_data[4]['user']['screen_name'] . ">" . $response_data[4]['user']['name'] . "</a>" ?></h4>
+    
+    <?php echo $response_data[4]['text'] ?>
+
+
+       <p> <?php echo date("l M j \- g:ia",strtotime($response_data['created_at'])); ?> </p>
+  </div>
+</div>
+
+
+      <div id="push"></div>
+    </div>
+
+    <div id="footer">
+      <div class="container">
+      </div>
+    </div>
+    <script src="http://code.jquery.com/jquery.js"></script>
+    <script src="bootstrap/js/bootstrap.min.js"></script>
+  </body>
+</html>
+
