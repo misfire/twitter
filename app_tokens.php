0fb742dc521dc0d0bb63a27e1eb905127b21a01a
Danny Chen <aznlucidx@gmail.com>
2013-06-04 17:14:13 -0700

    initial setup

---
+++ app_tokens.php
@@ -0,0 +1,10 @@
+<?php
+
+//OATH tokens
+
+$consumer_key = 'zZfosXqiJtfg4mSCraog';
+$consumer_secret = 'fOovLCOqh1eGW0S5loLrXIwpuJnipPJbGd8cVY0tc';
+$user_token = '14993946-w3QgA5cpYi3va2XWI6QunJ3giVld9oXNneOlxseDf';
+$user_secret = 'NEhmjZNofrT6MmAneIFs5F8m8987hZt1fpQa1f5KBpE';
+
+?>
