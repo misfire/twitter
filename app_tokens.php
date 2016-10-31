2b71b059fd9d623fe9104f346229c846fc463b64
Danny <danny@Dannys-MacBook-Pro.local>
2016-10-31 05:38:54 -0700

    Update api tokens

--- app_tokens.php
+++ app_tokens.php
@@ -4,7 +4,7 @@
 
 $consumer_key = 'zZfosXqiJtfg4mSCraog';
 $consumer_secret = 'fOovLCOqh1eGW0S5loLrXIwpuJnipPJbGd8cVY0tc';
-$user_token = '14993946-w3QgA5cpYi3va2XWI6QunJ3giVld9oXNneOlxseDf';
-$user_secret = 'NEhmjZNofrT6MmAneIFs5F8m8987hZt1fpQa1f5KBpE';
+$user_token = '14993946-1OfJufIHQV6ROTmnjlwstAnRjM56bdq7s1sQcj2MT';
+$user_secret = 'os6ZiPDt8RIJy19SGx5ZuPf6N9OIdQd9HBVK0zq0vqEiM';
 
 ?>
